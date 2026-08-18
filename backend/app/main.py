d84db766a4d49e4631daaa282bb5f53c08c258a4
saitejach07 <chatarajupalli_sai@srmap.edu.in>
2026-08-18 20:24:51 +0530

    cors changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="AI RAG Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://ai-rag-workbench.vercel.app",],
     allow_origin_regex=r"^http://(localhost|127\.0\.0\.1):\d+$",
     allow_credentials=True,
     allow_methods=["*"],
